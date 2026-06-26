8bc7d94790aca0f47d24e058fb4501b41cfa927b
Hetal-Jain-1405 <hetaljain1423@gmail.com>
2026-06-26 22:05:34 +0530

    CORS are added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
 from .router import user, movies, booking, payment
 
 from . import models
 from .database import engine
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
