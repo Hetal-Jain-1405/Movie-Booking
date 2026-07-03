b24dd3286d63cbc21a00738decebf1c4caa9dd0c
Hetal-Jain-1405 <hetaljain1423@gmail.com>
2026-07-03 19:54:56 +0530

    JWT token is accessible while signing up

--- backend/app/router/user.py
+++ backend/app/router/user.py
@@ -10,7 +10,7 @@ router = APIRouter(
 )
 
 
-@router.post('/new', response_model = schemas.UserResponse)
+@router.post('/new')
 def new_user(user: schemas.User, db: Session = Depends(database.get_db)):
     
     hashed_password = utils.hash(user.password)
@@ -22,12 +22,16 @@ def new_user(user: schemas.User, db: Session = Depends(database.get_db)):
     db.commit()
     db.refresh(new_user)
     
-    return new_user
+    access_token = oauth2.create_access_token(data= {'user_id': new_user.id})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
 
 @router.post('/login', response_model = schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
-    user = db.query(models.User).filter(user_credentials.username == models.User.email).first()
+    user = ( db.query(models.User)
+            .filter(user_credentials.username == models.User.email)
+            .first() )
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= 'Invalid credentials')
