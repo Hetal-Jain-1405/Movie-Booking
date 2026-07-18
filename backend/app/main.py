e9972b2e8c21e53b5c63c529a8e1caff2e683b92
Hetal-Jain-1405 <hetaljain1423@gmail.com>
2026-07-18 18:57:36 +0530

    backend url update

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 
 origins = [
         "http://localhost:5173",
-        'https://movie-booking-pi-three.vercel.app'
+        'https://movie-booking-pi-three.vercel.app',
+        
 ]
 
 app.add_middleware(
